c003ab84d8ddd269ef03f418168ace696dc31933
Serhii L. <118447093+Inanim4te@users.noreply.github.com>
2025-06-18 21:09:06 +0700

    fix 02

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,49 +1,17 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import { renderPics } from './render-functions';
-
-const API_KEY = '33649719-b7fecbfe979c6e7e0b54f5aa7';
-
-export function fetchPics() {
-  const galleryEl = document.querySelector('.gallery');
-  const loaderEl = document.querySelector('.loader');
-  const inputEl = document.getElementById('searchQuery');
-
-  galleryEl.innerHTML = '';
-  loaderEl.style.display = 'block';
-
-  const value = inputEl.value;
-  inputEl.value = '';
-  const encodedValue = encodeURIComponent(value);
+export async function fetchPics(query, key) {
   const params = new URLSearchParams({
-    key: API_KEY,
-    q: encodedValue,
+    key: key,
+    q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
   });
-  const queryString = params.toString();
 
-  fetch(`https://pixabay.com/api/?${queryString}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
+  const response = await fetch(`https://pixabay.com/api/?${params}`);
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
 
-        return;
-      }
-      renderPics(data);
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
-      loaderEl.style.display = 'none';
-    });
+  const data = await response.json();
+  return data;
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,50 @@
 import SimpleLightbox from 'simplelightbox';
+import iziToast from 'izitoast';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
 import { fetchPics } from './js/pixabay-api';
+import { renderPics } from './js/render-functions';
 
 const formEl = document.getElementById('search-form');
+const inputEl = document.getElementById('searchQuery');
+const loaderEl = document.querySelector('.loader');
+const galleryEl = document.querySelector('.gallery');
+
+const API_KEY = '33649719-b7fecbfe979c6e7e0b54f5aa7';
 
 export const galleryLightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
-  captionDelay: '250',
+  captionDelay: 250,
 });
 
-formEl.addEventListener('submit', e => {
+formEl.addEventListener('submit', async e => {
   e.preventDefault();
-  fetchPics();
+
+  const query = inputEl.value.trim();
+  if (!query) return;
+  inputEl.value = '';
+
+  galleryEl.innerHTML = '';
+  loaderEl.style.display = 'block';
+
+  try {
+    const data = await fetchPics(query, API_KEY);
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      return;
+    }
+
+    renderPics(data);
+    galleryLightbox.refresh();
+  } catch (error) {
+    console.error(error);
+    iziToast.error({
+      message: 'Something went wrong. Please try again later.',
+    });
+  } finally {
+    loaderEl.style.display = 'none';
+  }
 });
